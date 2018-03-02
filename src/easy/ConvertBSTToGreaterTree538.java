2ba313f23e7dbcb7001fa5b41789db1df51771dc
Deng Xinliang <dxinl0307@gmail.com>
2018-03-02 17:42:24 +0800

    647/538

---
+++ src/easy/ConvertBSTToGreaterTree538.java
@@ -0,0 +1,45 @@
+public class ConvertBSTToGreaterTree538 {
+    /**
+     * Definition for a binary tree node.
+     **/
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        // [2,0,3,-4,1]
+        TreeNode root = new TreeNode(2);
+        TreeNode n0 = new TreeNode(0);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n_4 = new TreeNode(-4);
+        TreeNode n1 = new TreeNode(1);
+        root.left = n0;
+        root.right = n3;
+        n0.left = n_4;
+        n0.right = n1;
+        new ConvertBSTToGreaterTree538().convertBST(root);
+    }
+
+    public TreeNode convertBST(TreeNode root) {
+        getChildVal(root, 0);
+        return root;
+    }
+
+    private int getChildVal(TreeNode root, int parentVal) {
+        if (root == null) {
+            return 0;
+        }
+
+        root.val += getChildVal(root.right, parentVal) + parentVal;
+        if (root.left != null) {
+            return getChildVal(root.left, root.val);
+        }
+        return root.val;
+    }
+}
